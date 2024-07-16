5dd9b2d8ef0c0a92b22a1e8dc5cf55842fc2441d
Duvan Jaramillo <duvanlol02@gmail.com>
2024-07-16 15:40:12 -0500

    Organizar la estructura de carpetas

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es-CO">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba MVC</title>
+</head>
+<body>
+    <?php
+        include 'views/contact.php';
+    ?>
+</body>
+</html>
